d7c79cc5e068cf0567e8ba70dba088980af5ea3f
yannis.guyennon@etu.univ-lyon1.fr <yannis.guyennon@etu.univ-lyon1.fr>
2022-03-08 10:01:47 +0100

    delegate edit item

--- iOS/model/CategoryItemCell.swift
+++ iOS/model/CategoryItemCell.swift
@@ -8,10 +8,23 @@
 import Foundation
 import UIKit
 
+
+protocol CategoryItemCellDelegate: AnyObject{
+    func categoryItemCell(_ cell: CategoryItemCell, didEditFor: Category)
+}
+
 class CategoryItemCell: UITableViewCell{
 
 
     @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var editButton: UIImageView!
+    @IBOutlet weak var edit: UIButton!
+    
+    var category: Category!
+    
+    var delegate: CategoryItemCellDelegate!
+    
+    @IBAction func editItem(_ sender: Any) {
+        self.delegate.categoryItemCell(self, didEditFor: category)
+    }
     
 }
