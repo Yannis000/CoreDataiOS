cb731de7b37036a3facacb06b1b85b24a46d5cca
yannis.guyennon@etu.univ-lyon1.fr <yannis.guyennon@etu.univ-lyon1.fr>
2022-03-08 13:48:03 +0100

    edit fix

--- iOS/controller/AddEditPlaceViewController.swift
+++ iOS/controller/AddEditPlaceViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import PhotosUI
 
-class AddEditPlaceViewController: UIViewController, PHPickerViewControllerDelegate {
+class AddEditPlaceViewController: UIViewController, PHPickerViewControllerDelegate, UITextViewDelegate{
 
     var currentCategory: Category!
     var entete: String = ""
@@ -21,9 +21,13 @@ class AddEditPlaceViewController: UIViewController, PHPickerViewControllerDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = entete
-        self.imageView.image = UIImage(systemName: "plus")
+        self.imageView.image = UIImage(systemName: "photo")
         config.selectionLimit = 1
         config.filter = PHPickerFilter.images
+        desc.layer.borderWidth = 0.1
+        desc.layer.cornerRadius = 8
+        desc.layer.borderColor = UIColor.gray.cgColor
+        desc.delegate = self
         // Do any additional setup after loading the view.
         if(entete == "Edit"){
             placeTitle.text = landmark?.title
@@ -33,11 +37,17 @@ class AddEditPlaceViewController: UIViewController, PHPickerViewControllerDelega
             if let image = landmark?.image {
                 imageView.image = UIImage(data: image)
             }
-            
         }
     }
     
-    
+    func textViewDidChange(_ textView: UITextView) {
+        if(textView.text.isEmpty){
+            descPlaceholder.isHidden = false
+        }else {
+            descPlaceholder.isHidden = true
+        }
+    }
+
     @IBAction func addImage(_ sender: Any) {
         
         let pickerViewController = PHPickerViewController(configuration: config)
@@ -50,7 +60,8 @@ class AddEditPlaceViewController: UIViewController, PHPickerViewControllerDelega
     @IBOutlet weak var placeTitle: UITextField!
     @IBOutlet weak var latitude: UITextField!
     @IBOutlet weak var longitude: UITextField!
-    @IBOutlet weak var desc: UITextField!
+    @IBOutlet weak var desc: UITextView!
+    @IBOutlet weak var descPlaceholder: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func cancel(_ sender: Any) {
